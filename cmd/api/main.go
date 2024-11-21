7e71f341c001bf4d9d630ed0f0dc87a13042f72e
Bakhram <bakhram7493@gmail.com>
2024-11-21 16:34:16 +0600

    init config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Bakhram74/effective-mobile-test-work.git/config"
+)
 
 func main() {
-	fmt.Println("bismiLLah")
+	cfg, err := config.LoadConfig(".")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(cfg.DBSource)
 }
